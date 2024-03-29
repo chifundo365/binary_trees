53eb6be25a7d4b3cd170a8baad99b43d90177af8
= <=>
2024-03-29 13:27:03 -0700

    Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,8 +8,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
 	{
-		binary_tree_delete(tree->left);
 		free(tree);
+		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 	}
 }
