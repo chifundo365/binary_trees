e66f23a7f8f8aba0440b57d8dfc4c267e0b8bae6
= <=>
2024-03-29 13:15:56 -0700

    Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes the entire binary tree
+ * tree: pointer to the root of the binary tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		free(tree);
+		binary_tree_delete(tree->right);
+	}
+}
