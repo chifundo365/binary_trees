d39b6072192ddc8d3b71c818b89b68fd971c736f
= <=>
2024-03-29 17:16:21 -0700

    Insert Left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
 
-	if (new_node)
+	if (!new_node)
 		return (NULL);
 
 	if (parent->left)
