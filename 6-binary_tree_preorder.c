a737b9fb2fb29b788c28d105ea22d6553f7682fc
= <=>
2024-03-30 08:27:06 -0700

    Pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverses the binary tree using pre-order traversal
+ * @tree: pointer to the root of the binary tree
+ * @func: pointer to a function to call to print node value
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
