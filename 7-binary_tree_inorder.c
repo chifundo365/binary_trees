d169ba323405184bf97092cbac1d32fb9c23997f
= <=>
2024-03-30 08:48:48 -0700

    Post-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,6 +2,7 @@
 
 /**
  * binary_tree_inorder -  traverse the binary tree using inorder traversing
+ * @tree: pointer to the root of the binary tree
  * @func: pointer to a function to print the node value
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
@@ -11,6 +12,5 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		binary_tree_inorder(tree->left, func);
 		func(tree->n);
 		binary_tree_inorder(tree->right, func);
-		
 	}
 }
