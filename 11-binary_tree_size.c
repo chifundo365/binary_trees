da025a151f53d9de4050f69be2c4f3c09ff3c847
= <=>
2024-04-01 18:23:07 -0700

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/***
+/**
  * binary_tree_size - measures the size of a binary tree
  * @tree: node to measure the size
  * Return: size of the tree
@@ -9,6 +9,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return ((binary_tree_size(tree->left)) + 1 + 
+	return ((binary_tree_size(tree->left)) + 1 +
 			(binary_tree_size(tree->right)));
 }
