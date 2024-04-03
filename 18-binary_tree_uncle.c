4c0b9273d5a762e7effbba7802d08ee759a7b5f3
= <=>
2024-04-03 03:03:35 -0700

    Uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-	if (node->parent->parent->left == node->parent)    
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
 	return (node->parent->parent->left);
 }
